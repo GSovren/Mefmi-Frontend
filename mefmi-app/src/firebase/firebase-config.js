19bb425b8d906e39a8913fbd4b524a74454bf6cb
Stewart Ngozo <80474317+GSovren@users.noreply.github.com>
2025-09-12 11:56:25 +0200

    Update firebase-config.js

--- mefmi-app/src/firebase/firebase-config.js
+++ mefmi-app/src/firebase/firebase-config.js
@@ -1,1 +1,26 @@
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "mefmi-auth.firebaseapp.com",
+  projectId: "mefmi-auth",
+  storageBucket: "mefmi-auth.firebasestorage.app",
+  messagingSenderId: "855254845104",
+  appId: "1:855254845104:web:95d66800e67667db5a9d75",
+  measurementId: "G-R2HL6X23GS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app)
+
+export { app, auth };
